90c1acb3468a6f15644c6afd1d1c3b4005d3b6b4
VicLove25 <vlove@una.edu>
2025-03-13 12:15:30 -0500

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,136 +1,135 @@
 $(function () {
   console.log("ready!");
-const randomFacts = [
-  {
-    fact: "My random fact is that I have two brothers",
-    username: "ReedWillis",
-    
-  },
-  {
-    fact: "My fav color is purple.",
-    username: "RichieJY",
-    
-  },
-  {
-    fact: "I like to swim",
-    username: "beastcarp1",
-    
-  },
-  {
-    fact: "I broke my finger",
-    username: "JaxonTheCritic",
-    
-  },
-  {
-    fact: "My name is Victor, and I'm ambidextrous.",
-    username: "VicLove25",
-    
-  },
-  {
-    fact: "I can say my alphabet backwards",
-    username: "Olivia-Sharpston",
-  },
-  {
-    fact: "I am not famous",
-    username: "CoreyBut",
-    
-  },
-  {
-    fact: "I was born in Louisiana",
-    username: "tatumduthu",
-    
-  },
-  {
-    fact: "I have never had a Peanut Butter and Jelly Sandwich!",
-    username: "Kaden-Trousdale",
-    
-  },
-  {
-    fact: "I am allergic to cats",
-    username: "dgoodwin-maker",
-   
-  },
-  {
-    fact: "I like trains.",
-    username: "SSenecal01",
-    
-  },
-  {
-    fact: "I know the secret identity of beastcarp1",
-    username: "Dwells2003",
-    
-  },
-   {
-    fact: "I have a dog",
-    username: "Matthewwisee",
-    
-  },
-  {
-    fact: "I hate UNA parking",
-    username: "jacobfielder",
-    
-  },
-  {
-    fact: "I have 3 dogs",
-    useranme: "mandrews3",
-    
-  },
-   {
-    fact: "I'm adopted from China :D",
-    username: "carlycopley",
-    
-  },
-  {
-    fact: "I used to have an orange Hummer H3",
-    username: "ccantrell2",
-    
-  },
-  {
-    fact: "I play on the rugby team",
-    username: "carsonkerr",
-    
-  },
-   {
-    fact: "UNA basketball sold the bag",
-    username: "gageb3",
-    
-  },
-  {
-    fact: "The Great Wall of China is 13,000 miles long",
-    username: "CorbinEhlendt",
-    
-  },
-  {
-    fact: "I hate javascript.",
-    username: "hnguyen1995",
-    
-  },
-   {
-    fact: "I love the Bible verse Romans 5:8!",
-    username: "llemoine1230",
-    
-  },
-   {
-    fact: "I love running.",
-    username: "ehb2004",
-    
-  },
-   {
-    fact: "🦭🦭",
-    username: "Jinxler",
-    
-  },
-   {
-    fact: "I have eaten 1 pound of ground beef every day for the last 6 months.",
-    username: "gshew24",
-    
-  },
-   {
-    fact: "Still very less parking space in UNA",
-    username: "ArunArdor",
-    
-  }
-
   
-
-];
+  const randomFacts = [
+    {
+      fact: "My random fact is that I have two brothers",
+      username: "ReedWillis",
+      
+    },
+    {
+      fact: "My fav color is purple.",
+      username: "RichieJY",
+      
+    },
+    {
+      fact: "I like to swim",
+      username: "beastcarp1",
+      
+    },
+    {
+      fact: "I broke my finger",
+      username: "JaxonTheCritic",
+      
+    },
+    {
+      fact: "My name is Victor, and I'm ambidextrous.",
+      username: "VicLove25",
+      
+    },
+    {
+      fact: "I can say my alphabet backwards",
+      username: "Olivia-Sharpston",
+    },
+    {
+      fact: "I am not famous",
+      username: "CoreyBut",
+      
+    },
+    {
+      fact: "I was born in Louisiana",
+      username: "tatumduthu",
+      
+    },
+    {
+      fact: "I have never had a Peanut Butter and Jelly Sandwich!",
+      username: "Kaden-Trousdale",
+      
+    },
+    {
+      fact: "I am allergic to cats",
+      username: "dgoodwin-maker",
+     
+    },
+    {
+      fact: "I like trains.",
+      username: "SSenecal01",
+      
+    },
+    {
+      fact: "I know the secret identity of beastcarp1",
+      username: "Dwells2003",
+      
+    },
+     {
+      fact: "I have a dog",
+      username: "Matthewwisee",
+      
+    },
+    {
+      fact: "I hate UNA parking",
+      username: "jacobfielder",
+      
+    },
+    {
+      fact: "I have 3 dogs",
+      useranme: "mandrews3",
+      
+    },
+     {
+      fact: "I'm adopted from China :D",
+      username: "carlycopley",
+      
+    },
+    {
+      fact: "I used to have an orange Hummer H3",
+      username: "ccantrell2",
+      
+    },
+    {
+      fact: "I play on the rugby team",
+      username: "carsonkerr",
+      
+    },
+     {
+      fact: "UNA basketball sold the bag",
+      username: "gageb3",
+      
+    },
+    {
+      fact: "The Great Wall of China is 13,000 miles long",
+      username: "CorbinEhlendt",
+      
+    },
+    {
+      fact: "I hate javascript.",
+      username: "hnguyen1995",
+      
+    },
+     {
+      fact: "I love the Bible verse Romans 5:8!",
+      username: "llemoine1230",
+      
+    },
+     {
+      fact: "I love running.",
+      username: "ehb2004",
+      
+    },
+     {
+      fact: "🦭🦭",
+      username: "Jinxler",
+      
+    },
+     {
+      fact: "I have eaten 1 pound of ground beef every day for the last 6 months.",
+      username: "gshew24",
+      
+    },
+     {
+      fact: "Still very less parking space in UNA",
+      username: "ArunArdor",
+      
+    }
+  ];
+};
