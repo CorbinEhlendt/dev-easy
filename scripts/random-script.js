57e4e44be62b937f13c9d972944c65660160808a
Matthew Landon Andrews <149531388+mandrews3@users.noreply.github.com>
2025-03-13 11:39:19 -0500

    Update random-script.js

--- scripts/random-script.js
+++ scripts/random-script.js
@@ -8,9 +8,7 @@ $(function () {
           <h2 class="card-title"> ${randomFacts[index].fact} </h2>
           <div class="card-body">${randomFacts[index].username}</div>
           <div class="card-footer">
-           <button id="likeButton" onclick="incrementLikes()">Like</button>
-            <span id="likeCount">0</span> Likes
-            //<button class="btn btn-primary">game link</button>
+            <button class="btn btn-primary">game link</button>
           </div>
         </div>
       </div>
