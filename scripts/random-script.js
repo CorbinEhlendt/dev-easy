f6745923d8e1989f35dbc28f025d090d9ce88906
Corbin Ehlendt <cehlendt@una.edu>
2025-03-13 10:56:10 -0500

    Update random-script.js

--- scripts/random-script.js
+++ scripts/random-script.js
@@ -1,12 +1,12 @@
 $(function () {
   console.log("ready!");
 
-  $.each(golfGames, (index, data) => {
+  $.each(randomFacts, (index, data) => {
     $("#gameRow").append(`
     <div class="col-sm-5 mx-auto mt-5">
         <div class="card">
-          <h2 class="card-title"> ${golfGames[index].fact} </h2>
-          <div class="card-body">${golfGames[index].author}</div>
+          <h2 class="card-title"> ${randomFacts[index].fact} </h2>
+          <div class="card-body">${randomFacts[index].author}</div>
           <div class="card-footer">
             <button class="btn btn-primary">game link</button>
           </div>
